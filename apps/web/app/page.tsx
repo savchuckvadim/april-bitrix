3508142bf51efc700b512a9bfc4d21275a403dfd
savchuckvadim <savchuckvadim@gmail.com>
2025-04-09 11:34:39 +0300

    initial commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@workspace/ui/components/button"
+
+export default function Page() {
+  return (
+    <div className="flex items-center justify-center min-h-svh bg-foreground">
+      <div className="flex flex-col items-center justify-center gap-4 bg-foreground">
+        <h1 className="text-2xl font-bold">Hello World</h1>
+        <Button size="sm">Button</Button>
+      </div>
+    </div>
+  )
+}

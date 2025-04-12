4279a20e30c340fb09cfc3b78b9ebe3997bbf615
savchuckvadim <savchuckvadim@gmail.com>
2025-04-12 22:10:17 +0300

    add pace

---
+++ apps/bitrix/app/head.tsx
@@ -0,0 +1,12 @@
+// app/head.tsx
+export default function Head() {
+    return (
+        <>
+            <title>Конструктор КП</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pace-js@1/themes/blue/pace-theme-flash.css" />
+            <link rel="stylesheet" href="/bounce.css" />
+
+            <script src="https://cdn.jsdelivr.net/npm/pace-js@1/pace.min.js"></script>
+        </>
+    );
+}

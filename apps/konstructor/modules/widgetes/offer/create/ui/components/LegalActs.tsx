bf29dc2bedec5085ac1896855c0adba93bcfa433
savchuckvadim <savchuckvadim@gmail.com>
2025-06-02 18:44:08 +0300

    add konstructor page

---
+++ apps/konstructor/modules/widgetes/offer/create/ui/components/LegalActs.tsx
@@ -0,0 +1,10 @@
+export function LegalActs() {
+    return (
+      <div className="space-y-2 p-4">
+        <h3 className="text-md font-semibold">Нормативно-правовые акты</h3>
+        <p>Законодательство России</p>
+        <p>Документы, регулирующие вопросы ведения РФ и совместного ведения РФ и субъектов РФ.</p>
+      </div>
+    );
+  }
+  

91691256d497c936b7f074ad7047195f1ffe8fd0
Vadim Savchuk <savchuckvadim@gmail.com>
2025-08-31 09:28:02 +0300

    fxd expired

--- apps/konstructor/modules/entities/offer-template-block/ui/letter/render-pdf/RenderOfferLetter.tsx
+++ apps/konstructor/modules/entities/offer-template-block/ui/letter/render-pdf/RenderOfferLetter.tsx
@@ -5,31 +5,31 @@ import RenderFormattedText from "./RenderFormattedText";
 import { IOfferTemplate } from "@/modules/entities/offer-template/type/offer-template.type";
 
 const RenderOfferLetter: FC<{
-  block: IOfferBlockLetter;
-  template: IOfferTemplate;
+    block: IOfferBlockLetter;
+    template: IOfferTemplate;
 }> = ({ block, template }) => {
-  const colors = template.colors;
-  const backgroundColor = colors.background.value;
+    const colors = template.colors;
+    const backgroundColor = colors.background.value;
 
-  return (
-    <div
-      className={`flex my-2 p-3`}
-      style={{
-        backgroundColor: backgroundColor,
-        height: `${block.height}mm`,
-      }}
-    >
-      <div className="w-full">
-        <RenderFormattedText
-          text={block.content.text}
-          textColor={colors.text.value}
-          redColor={colors.accentText.value}
-          titleColor={colors.accent.value}
-          font={template.font.value}
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className={`flex my-2 p-3`}
+            style={{
+                backgroundColor: backgroundColor,
+                height: `${block.height}mm`,
+            }}
+        >
+            <div className="w-full">
+                <RenderFormattedText
+                    text={block.content.text}
+                    textColor={colors.text.value}
+                    redColor={colors.accentText.value}
+                    titleColor={colors.accent.value}
+                    font={template.font.value}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default RenderOfferLetter;
--- apps/kpi-sales/modules/app/model/AppSlice.ts
+++ apps/kpi-sales/modules/app/model/AppSlice.ts
@@ -5,97 +5,97 @@ import type { BXUser } from "@workspace/bx";
 
 export type AppState = typeof initialState;
 export enum APP_DEP {
-  SALES = "sales",
-  SERVICE = "service",
+    SALES = "sales",
+    SERVICE = "service",
 }
 const initialState = {
-  domain: "",
-  app: APP_TYPE.REPORT as APP_TYPE,
-  bitrix: {
-    user: null as BXUser | null,
-  },
-  client: {
-    id: "",
-    name: "",
-    isExpired: false,
-    isPunished: false,
-  },
+    domain: "",
+    app: APP_TYPE.REPORT as APP_TYPE,
+    bitrix: {
+        user: null as BXUser | null,
+    },
+    client: {
+        id: "",
+        name: "",
+        isExpired: false,
+        isPunished: false,
+    },
 
-  department: APP_DEP.SERVICE,
-  portal: null as Portal | null,
-  initialized: false,
-  isLoading: false,
-  error: {
-    status: false as boolean,
-    message: "" as string,
-  },
+    department: APP_DEP.SERVICE,
+    portal: null as Portal | null,
+    initialized: false,
+    isLoading: false,
+    error: {
+        status: false as boolean,
+        message: "" as string,
+    },
 };
 export interface InitReport {
-  domain: string;
-  user: BXUser;
+    domain: string;
+    user: BXUser;
 }
 const appSlice = createSlice({
-  name: "app",
-  initialState,
-  reducers: {
-    setAppData: (
-      state: AppState,
-      action: PayloadAction<InitReport>,
-      //   {
-      //     domain: string;
-      //     user: BXUser | null;
+    name: "app",
+    initialState,
+    reducers: {
+        setAppData: (
+            state: AppState,
+            action: PayloadAction<InitReport>,
+            //   {
+            //     domain: string;
+            //     user: BXUser | null;
 
-      //   }
-    ) => {
-      const payload = action.payload;
-      state.domain = payload.domain;
-      state.bitrix.user = payload.user;
-      state.initialized = true;
-      if (state.domain === "gsirk.bitrix24.ru") {
-        state.client.isExpired = true;
-      }
-    },
+            //   }
+        ) => {
+            const payload = action.payload;
+            state.domain = payload.domain;
+            state.bitrix.user = payload.user;
+            state.initialized = true;
+            //   if (state.domain === "gsirk.bitrix24.ru") {
+            //     state.client.isExpired = true;
+            //   }
+        },
 
-    setInitializedSuccess: (state: AppState, action: PayloadAction<{}>) => {
-      state.initialized = true;
+        setInitializedSuccess: (state: AppState, action: PayloadAction<{}>) => {
+            state.initialized = true;
+        },
+        setInitializedError: (
+            state: AppState,
+            action: PayloadAction<{
+                errorMessage: string;
+            }>,
+        ) => {
+            state.initialized = true;
+            state.error.status = true;
+            state.error.message = action.payload.errorMessage;
+        },
+        setCleanError: (
+            state: AppState,
+            action: PayloadAction<{
+                errorMessage: string;
+            }>,
+        ) => {
+            state.error.status = false;
+            state.error.message = "";
+        },
+        setPortal: (
+            state: AppState,
+            action: PayloadAction<{
+                portal: Portal;
+            }>,
+        ) => {
+            state.portal = action.payload.portal;
+        },
+        reload: (state: AppState, action: PayloadAction) => {
+            state.initialized = false;
+        },
+        loading: (state: AppState, action: PayloadAction<{ status: boolean }>) => {
+            state.isLoading = action.payload.status;
+        },
+        setExpiredClient: (state: AppState, action: PayloadAction<{ isExpired: boolean }>) => {
+            state.client.isExpired = action.payload.isExpired;
+        },
     },
-    setInitializedError: (
-      state: AppState,
-      action: PayloadAction<{
-        errorMessage: string;
-      }>,
-    ) => {
-      state.initialized = true;
-      state.error.status = true;
-      state.error.message = action.payload.errorMessage;
-    },
-    setCleanError: (
-      state: AppState,
-      action: PayloadAction<{
-        errorMessage: string;
-      }>,
-    ) => {
-      state.error.status = false;
-      state.error.message = "";
-    },
-    setPortal: (
-      state: AppState,
-      action: PayloadAction<{
-        portal: Portal;
-      }>,
-    ) => {
-      state.portal = action.payload.portal;
-    },
-    reload: (state: AppState, action: PayloadAction) => {
-      state.initialized = false;
-    },
-    loading: (state: AppState, action: PayloadAction<{ status: boolean }>) => {
-      state.isLoading = action.payload.status;
-    },
-    setExpiredClient: (state: AppState, action: PayloadAction<{ isExpired: boolean }>) => {
-      state.client.isExpired = action.payload.isExpired;
-    },
-  },
 });
 
 export const appReducer = appSlice.reducer;

bf9ab670c3d88a3257abe4d1d17701f554e733a6
Vadim Savchuk <savchuckvadim@gmail.com>
2025-12-26 19:44:51 +0300

    add stor log

--- apps/bitrix/modules/app/providers/AppProvider.tsx
+++ apps/bitrix/modules/app/providers/AppProvider.tsx
@@ -3,9 +3,16 @@ import { Provider } from 'react-redux';
 import { store } from '../model/store';
 import { ErrorBoundary } from './ErrorBoundary';
 import App from '../ui/App';
+import { useEffect } from 'react';
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
     console.log('AppProvider');
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            (window as any).store = store;
+            console.log('store', (window as any).store);
+        }
+    }, []);
     return (
         <Provider store={store}>
             <ErrorBoundary>

7dcf7b2dbb3c3ef1df470364d27360e0e752e896
Vadim Savchuk <savchuckvadim@gmail.com>
2025-12-26 14:27:47 +0300

    add enabled

--- apps/bitrix/modules/features/bx-install/lib/hook/bx-install.hook.ts
+++ apps/bitrix/modules/features/bx-install/lib/hook/bx-install.hook.ts
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { bxInstallHelper, getInstallStatus, getPlacementList } from "../api/bx-install.helper";
 import { useQuery } from "@tanstack/react-query";
 
 export const useBxInstall = () => {
     console.log('useBxInstall');
-
+    const bxReady = useBxReady();
+    console.log('bxReady');
+    console.log(bxReady);
     const query= useQuery<any, Error>({
         queryKey: ['install'],
         queryFn: bxInstallHelper,
+        enabled: bxReady, // ❗ КРИТИЧНО
         retry: false,          // ❗ важно для install
         refetchOnWindowFocus: false,
         refetchOnMount: false,
@@ -21,4 +24,29 @@ export const useBxInstall = () => {
         isLoading: query.isLoading,
         error: query.error,
     };
+};
+
+
+export const useBxReady = () => {
+    const [ready, setReady] = useState(false);
+
+    useEffect(() => {
+        if (typeof window === 'undefined') return;
+
+        if ((window as any).BX24) {
+            setReady(true);
+            return;
+        }
+
+        const interval = setInterval(() => {
+            if ((window as any).BX24) {
+                setReady(true);
+                clearInterval(interval);
+            }
+        }, 50);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return ready;
 };

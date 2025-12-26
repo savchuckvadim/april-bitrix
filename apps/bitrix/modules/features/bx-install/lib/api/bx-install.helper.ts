d4a33126456dcb23375d6fc71537fc3d115d5184
Vadim Savchuk <savchuckvadim@gmail.com>
2025-12-26 15:19:16 +0300

    testing bx library

--- apps/bitrix/modules/features/bx-install/lib/api/bx-install.helper.ts
+++ apps/bitrix/modules/features/bx-install/lib/api/bx-install.helper.ts
@@ -1,31 +1,46 @@
-import { getBxService } from '@workspace/api';
-export const bxInstallHelper = async () => {
-    console.log('bxInstallHelper');
-    const BX24 = await getBxService();
-    console.log('BX24');
+// import { getBxService } from '@workspace/api';
+import { Bitrix } from '@workspace/bitrix';
 
-    const installFinish = await BX24.installFinish();
-    const data = installFinish.getData().result;
-    console.log('data installFinish');
+export const bxInstallHelper = async () => {
+    // console.log('bxInstallHelper');
+    // const BX24 = await getBxService();
+    // console.log('BX24');
+
+    // const installFinish = await BX24.installFinish();
+    // const data = installFinish.getData().result;
+    // console.log('data installFinish');
+    // console.log(data);
+    // // Проверка, что приложение реально установлено
+    // await getInstallStatus(); // если упадёт — кинет ошибку
+    // return data;
+
+    const bitrix = await Bitrix.start('', {
+        ID: 0,
+        NAME: '',
+        LAST_NAME: '',
+        EMAIL: '',
+
+    });
+    bitrix.api.getInitializedData();
+    const data = bitrix.api.getInitializedData();
+    console.log('data');
     console.log(data);
-    // Проверка, что приложение реально установлено
-    await getInstallStatus(); // если упадёт — кинет ошибку
     return data;
 };
 
 
 export const getPlacementList = async () => {
-    const BX24 = await getBxService();
-    const list = await BX24.callMethod('placement.list');
-    return list;
+    // const BX24 = await getBxService();
+    // const list = await BX24.callMethod('placement.list');
+    // return list;
 };
 
 
 export const getInstallStatus = async () => {
-    const BX24 = await getBxService();
-    const status = await BX24.callMethod('app.info');
-    const data = status.getData().result;
-    console.log('data app.info');
-    console.log(data);
-    return data;
+    // const BX24 = await getBxService();
+    // const status = await BX24.callMethod('app.info');
+    // const data = status.getData().result;
+    // console.log('data app.info');
+    // console.log(data);
+    // return data;
 };
